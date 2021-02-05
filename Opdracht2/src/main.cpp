dc3b218bd6aff1bf4b8baf4dac4385103b3334cb
StarboyCasper <casperleitner@outlook.com>
2021-02-05 02:22:14 +0100

    (v0.9) Added Visual representation of TrafficLight

---
+++ Opdracht2/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "../include/TrafficLight.h"
+#include "../include/TrafficLightController.h"
+
+int main()
+{
+	auto trafficLight = TrafficLight();
+
+	// ===================================
+	// Setting one traffic light at a time
+	// ===================================
+		
+	trafficLight = TrafficLightController::SetActiveColour(trafficLight, Lamp::lamp_colours::red);
+	TrafficLightController::DrawTrafficLight(trafficLight);
+	trafficLight = TrafficLightController::SetActiveColour(trafficLight, Lamp::lamp_colours::orange);
+	TrafficLightController::DrawTrafficLight(trafficLight);
+	trafficLight = TrafficLightController::SetActiveColour(trafficLight, Lamp::lamp_colours::green);
+	TrafficLightController::DrawTrafficLight(trafficLight);
+
+	// =======================================
+	// Setting multiple traffic lights at once
+	// =======================================
+	
+	auto colours = new std::vector<Lamp::lamp_colours>();
+	colours->push_back(Lamp::lamp_colours::red);
+	colours->push_back(Lamp::lamp_colours::orange);
+	colours->push_back(Lamp::lamp_colours::green);
+	trafficLight = TrafficLightController::SetActiveColours(trafficLight, *colours);
+	TrafficLightController::DrawTrafficLight(trafficLight);
+}
