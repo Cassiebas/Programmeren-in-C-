e145f9023faa93a86fa2db573ff4f8760c1e8bd8
StarboyCasper <casperleitner@outlook.com>
2021-02-11 14:20:08 +0100

    (v0.9)Added final lamps with orange light logic

---
+++ Opdracht3/include/blit.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+class blit
+{
+	public:
+		SDL_Texture *load_texture(char *filename, SDL_Renderer *renderer);
+		void background(SDL_Texture *txtr, SDL_Renderer *renderer);
+		void angled(SDL_Texture *txtr, int x, int y, float angle, int zoom, SDL_Renderer *renderer);
+};
